b85f804d83ae21d9f3b7ab8a8900668240d963af
QtyPython2020 <htm131016@gmail.com>
2024-11-17 20:36:05 +0100

    First draft

---
+++ similarities.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+@author: UnicornOnAzur
+
+"""
+
+def _check_input(set1: set, set2: set) -> bool:
+    pass
+
+
+def overlap_coefficient(set1: set, set2: set):
+    pass
+
+
+def jaccard_coefficient(set1: set, set2: set):
+    pass
+
+
+def cosine_coefficient(set1: set, set2: set):
+    pass
+
+
+def demo():
+    pass
+
+if __name__ == "__main__":
+    demo()
